8c9cecc604ac3695f1c1cc5753fe96cd9a28fc8b
SambhuPNS <sambhusubramannian@gmail.com>
2023-10-13 19:59:23 +0530

    added 31 and 32

---
+++ hands2/31b.c
@@ -0,0 +1,23 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<fcntl.h>
+
+int main()
+{
+        union semum
+        {
+                int val;        //value for SETVAL
+        }arg;
+
+        int key, semid;
+
+        key = ftok(".", 'b');
+        semid = semget(key, 1, IPC_CREAT | 0644);
+        arg.val = 2;    //1 for binary, >1 for counting semaphore
+
+        semctl(semid, 0, SETVAL, arg);
+
+        return 0;
+} 
